427d53d978eeb521f52380c8a0eae0a876b437d7
Bohdan <makohoniuk.bo@gmail.com>
2022-07-30 15:26:22 +0300

    ДЗ 3\41

--- js/script.js
+++ js/script.js
@@ -85,8 +85,56 @@
 //введёт число больше 100, либо не нажмет кнопку
 //Отмена в prompt
 
-let number = Number(prompt("ВВедите число больше 100!!!"));
-while (number < 100 && number !== null) {
-  number = Number(prompt("ВВедите число больше 100!!!"));
-}
-console.log(number);
+// let number = Number(prompt("ВВедите число больше 100!!!"));
+// while (number < 100 && number !== null) {
+//   number = Number(prompt("ВВедите число больше 100!!!"));
+// }
+// console.log(number);
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    for (const potion of this.potions) {
+      if (newPotion.name === potion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+      }
+    }
+    this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+    for (let i = 0; i < this.potions.length; i += 1){
+      
+      if (this.potions[i].name === potionName) {
+        
+        this.potions.splice(i, 1)
+        return;
+      }
+      
+    }
+return `Potion ${potionName} is not in inventory!`;
+ 
+  },
+  updatePotionName(oldName, newName) {
+    for (const potion of this.potions) {
+      if (potion.name === oldName) {
+        potion.name = newName
+        return;
+      } 
+    }
+      return `Potion ${oldName} is not in inventory!`;
+    
+
+   
+  },
+  // Change code above this line
+};
+ 
+atTheOldToad.removePotion("Dragon breath")
